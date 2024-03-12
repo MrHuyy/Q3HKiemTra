fcdfb14e3e26928873464e0a27adc6436811e3f3
MrHuyy <ngngochuye1@gmail.com>
2024-03-12 14:42:32 +0700

    A commit

--- Q3H/Form1.cs
+++ Q3H/Form1.cs
@@ -1,4 +1,4 @@
-namespace Q3H
+﻿namespace Q3H
 {
     public partial class Form1 : Form
     {
@@ -15,6 +15,28 @@ namespace Q3H
         private void lb1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassWord.Text;
+
+            if (username == "admin" && password == "admin")
+            {
+                // Đăng nhập thành công
+                MessageBox.Show("Đăng nhập thành công!");
+
+                // Chuyển sang form khác
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Đăng nhập thất bại
+                MessageBox.Show("Đăng nhập thất bại!");
+            }
         }
     }
 }
